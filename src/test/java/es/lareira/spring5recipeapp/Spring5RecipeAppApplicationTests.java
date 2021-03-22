5c4c6036e2abc5ff79d99f39aa9f767536250855
Carlos Bouzón García <carlosbga@ext.inditex.com>
2021-03-22 21:11:35 +0100

    Initial Commit

---
+++ src/test/java/es/lareira/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,14 @@
+package es.lareira.spring5recipeapp;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
