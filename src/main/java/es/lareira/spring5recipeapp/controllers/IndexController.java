c8f5aa0caf3c763ef75ba35f412f877dc0bb78a0
Carlos Bouzón García <carlosbga@ext.inditex.com>
2021-06-27 23:11:34 +0200

    Add index controller

---
+++ src/main/java/es/lareira/spring5recipeapp/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package es.lareira.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+  @RequestMapping({"", "/", "/index.html"})
+  public static String getIndexPage() {
+    return "index";
+  }
+}
