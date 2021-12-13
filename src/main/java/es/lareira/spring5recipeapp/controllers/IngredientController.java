f2dd5c286e8dfebda5e73bba9dbd12e97dd5ecae
Your Name <you@example.com>
2021-12-13 22:49:57 +0100

    Add difficulty in recipe update form

--- src/main/java/es/lareira/spring5recipeapp/controllers/IngredientController.java
+++ src/main/java/es/lareira/spring5recipeapp/controllers/IngredientController.java
@@ -12,6 +12,7 @@ import java.text.MessageFormat;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,14 +34,14 @@ public class IngredientController {
     this.unitOfMeasurementService = unitOfMeasurementService;
   }
 
-  @RequestMapping("/recipe/{id}/ingredients")
+  @GetMapping("/recipe/{id}/ingredients")
   public String listIngredients(@PathVariable String id, Model model) {
     Recipe recipe = recipeService.findRecipeById(Long.valueOf(id));
     model.addAttribute("recipe", recipe);
     return "recipe/ingredients/list";
   }
 
-  @RequestMapping("/recipe/{recipeId}/ingredients/{ingredientId}/update")
+  @GetMapping("/recipe/{recipeId}/ingredients/{ingredientId}/update")
   public String updateRecipeIngredient(@PathVariable String recipeId,
       @PathVariable String ingredientId, Model model) {
     IngredientCommand ingredientCommand = ingredientService.findByRecipeIdAndIngredientId(
@@ -51,7 +52,7 @@ public class IngredientController {
     return "recipe/ingredients/updateForm";
   }
 
-  @RequestMapping("/recipe/{recipeId}/ingredients/new")
+  @GetMapping("/recipe/{recipeId}/ingredients/new")
   public String newRecipeIngredient(@PathVariable String recipeId, Model model) {
     RecipeCommand recipeCommandById = recipeService.findRecipeCommandById(Long.valueOf(recipeId));
     assert recipeCommandById != null;
